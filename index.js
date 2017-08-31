c70b629e2f785a4d7d2b11916917799992250044
Alexey Kornilov <alexey@Alexeys-MacBook.local>
2017-08-31 23:44:23 +0300

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const MyForm = (() => {
+    let self = {}
+
+    const fioWordCOunt = 3;
+    const emailAllowDomain = ['ya.ru', 'yandex.ru', 'yandex.ua', 'yandex.by', 'yandex.kz', 'yandex.com'];
+    const phoneBegin = '+7';
+    const phoneDigitCount = 70;
+
+    return self;
+})();
+console.log('test');
